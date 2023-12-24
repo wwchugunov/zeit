c55c62d848d61318f82e96ead88e27fda6f18eb0
wwchugunov <w.w.chugunov>
2023-12-24 14:51:51 +0300

    add video

--- js/index.js
+++ js/index.js
@@ -13,21 +13,17 @@ SmoothScroll({
 
 // script.js
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-  
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-  
-        if (targetElement) {
-          window.scrollTo({
-            top: targetElement.offsetTop - document.querySelector('nav').offsetHeight,
-            behavior: 'smooth'
-          });
-        }
-      });
+
+
+  <script>
+  $(document).ready(function() {
+    $("[data-fancybox]").fancybox({
+      animationEffect: "fade",
+      transitionEffect: "slide",
+      thumbs: {
+        autoStart: true,
+        axis: "y"
+      }
     });
   });
-  
+</script>
